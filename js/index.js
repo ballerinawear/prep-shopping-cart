373dc5112e61ab26407c90fa28e922649b4d4772
G. Takanishi <22designshi@gmail.com>
2016-02-01 01:15:38 -1000

    shopping cart

--- js/index.js
+++ js/index.js
@@ -1,67 +1,101 @@
 window.onload = function(){};
 
 var cartItems =[
-  { "name": "Lusicious Jello Mix", "description": ["Very Elegant", "Trending item", "Come in Purple"], "price": 80.65 },
-  { "name": "Tarnished Standing Desk", "description": ["Modular", "Works for both Tall and Loud People", "Smells like Productivity"], "price": 1654.99},
-  { "name": "Hand-made Hand Grenades", "description": ["Such gift!", "Much boom!", "Very safe for kids"], "price": 10.44},
-  { "name": "Pan-fried Cookie Dough", "description": ["Chocolate", "Family-size", "Hot Mess"], "price": 16.99 },
-  { "name": "Fancy Dress Hanger", "description": ["Keep organized", "On Sale"], "price": 67.32 },
-  { "name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", "Hugs not drugs", "As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99 },
+  {"image": "images/jello1.jpg", "name": "Lusicious Jello Mix", "description": ["Very Elegant", "Trending item", "Come in Purple"], "price": 80.65, },
+  { "image": "images/cart.svg", "name": "Tarnished Standing Desk", "description": ["Modular", "Works for both Tall and Loud People", "Smells like Productivity"], "price": 1654.99},
+  {"image": "images/cart.svg", "name": "Hand-made Hand Grenades", "description": ["Such gift!", "Much boom!", "Very safe for kids"], "price": 10.44},
+  { "image": "images/cart.svg","name": "Pan-fried Cookie Dough", "description": ["Chocolate", "Family-size", "Hot Mess"], "price": 16.99 },
+  { "image": "images/cart.svg","name": "Fancy Dress Hanger", "description": ["Keep organized", "On Sale"], "price": 67.32 },
+  { "image": "images/cart.svg","name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", "Hugs not drugs", "As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99 },
 ];
 
 var containerElement = document.getElementById('container');
 
 for (var i=0; i<cartItems.length; i++){
-	// console.log(cartItems[i].name);
-	// console.log(cartItems[i].description);
-	// console.log(cartItems[i].price);
 
-// var itemName = cartItems[i].name;
-// var itemDescription = cartItems[i].description;
-// var itemPrice = cartItems[i].price;
+	// var itemName = cartItems[i].name;
+	// var itemDescription = cartItems[i].description;
+	// var itemPrice = cartItems[i].price;
+
 
 var divItem = document.createElement('div');
-	divItem.className = ('item');
-	divItem.innerHTML=cartItems[i].name;
+	divItem.className = ('itemName');
+	//divItem.innerHTML=cartItems[i].name;
 	containerElement.appendChild(divItem);
 
-var divDescription = document.createElement('div');
-	divDescription.className = ('description');
-	divDescription.innerHTML=cartItems[i].description;
-	containerElement.appendChild(divDescription);
+var divImage = document.createElement('div');
+	divImage.className = ('cartStuff');
+	divImage.innerHTML = "<img src=" + cartItems[i].image + "/>";
+	document.getElementsByClassName('itemName')[i].appendChild(divImage);
 
-var divPrice = document.createElement('div');
-	divPrice.className = ('price');
-	divPrice.innerHTML= "$ " + cartItems[i].price;
-	containerElement.appendChild(divPrice);
+var divDescription = document.createElement('div');
+	divDescription.className = ('itemDescription');
+	divDescription.innerHTML=cartItems[i].name + "<br>" + cartItems[i].description;
+	document.getElementsByClassName('itemName')[i].appendChild(divDescription);
+
+for (var j=0; j<10; j++){
+var dropDown = document.createElement('select');
+	dropDown.className = ('dropDown');
+	dropDown.innerHTML = "<option>" + [j] + "</option>";
+	document.getElementsByClassName('itemName')[i].appendChild(dropDown);
 }
 
-var divLine = docuemnt.createElement('div');
+var divPrice = document.createElement('div');
+	divPrice.className = ('itemPrice');
+	divPrice.innerHTML= "$ " + cartItems[i].price;
+	document.getElementsByClassName('itemName')[i].appendChild(divPrice);
+
+
+var divLine = document.createElement('div');
 	divLine.className = "line";
 	divLine.innerHTML = '';
-	containerElement.appendChild(divLine);
+	document.getElementsByClassName('itemName')[i].appendChild(divLine);
+} 
 
-
-var divImage = document.createElement('img');
-	
-	divImage.src = images/cart.svg;
-	divImage.innerHTML = divImage.src;
-	containerElement.appendChild(divImage);
-/*var divImage = document.createElement('img');
-	divImage.className = ('image');
-	img.src = "images/cart.svg";
-	divImage.innerHTML=img.src;
-	document.getElementById('top').appendChild(divImage);
-	//src.appendChild(divImage);
-	containerElement.appendChild(divImage);*/
-
-/*function image() {
-	var img = document.createElement("IMG");
-	img.src = "images/cart.svg";
-	document.getElementById('image').appendChild(img);*/
+var subtotal = document.createElement('div');
+	subtotal.className = ('subtotal');
+	subtotal.innerHTML="<p>Subtotal</p>";
+	containerElement.appendChild(subtotal);
 
 
 
+
+
+// var total = 0;
+// var addItUp = [];
+
+// var cartItemTotal = function(subtotal){
+// 	for (var i=0; i<cartItems.length; i++){
+		
+// 		var itemPrice = cartItems[i].price;
+// 		addItUp.push(itemPrice);
+// 	}
+// };
+
+// var sum = function(subTotal){
+// for (var i=0; i<addItUp.length; i++){
+
+// 	subTotal = subTotal + addItUp[i];
+// }
+// };
+
+// var myTotal = subTotal;
+
+// var divTotal = document.createElement('div');
+// 	divTotal.className = ('total');
+// 	divTotal.innerHTML= sum;
+// 	containerElement.appendChild(divTotal);
+
+
+// getElementByClass('quantity').addEventListener('click', function(){
+//  myFunction(p1, p2);
+
+// });
+
+// function myFunction(a, b) {
+//     var result = a * b;
+//     document.getElementByClass("price").innerHTML = result;
+// }
 
 
 
